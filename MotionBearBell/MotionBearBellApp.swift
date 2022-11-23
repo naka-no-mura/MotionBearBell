fc883f1dd79d70e5d1795227997674898fa6f9dd
naka-no-mura <nakanomura.ryu@gmail.com>
2022-11-23 10:21:36 +0900

    Initial Commit

---
+++ MotionBearBell/MotionBearBellApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotionBearBellApp.swift
+//  MotionBearBell
+//
+//  Created by r.nakamura on 2022/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MotionBearBellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
